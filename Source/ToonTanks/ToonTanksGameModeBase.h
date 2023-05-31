b7d7404a634345e967803c96fbbdad22b94c6bdd
ryanchall99 <rileychall@hotmail.co.uk>
2023-05-31 19:24:20 +0100

    Starting Project
    Fresh Start

---
+++ Source/ToonTanks/ToonTanksGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToonTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API AToonTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
