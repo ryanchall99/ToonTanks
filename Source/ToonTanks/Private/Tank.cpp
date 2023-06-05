72ed0c683f4c4945d293201d10258414ba64a0a4
ryanchall99 <rileychall@hotmail.co.uk>
2023-06-05 16:28:54 +0100

    Added Enhanced Input System
    - Added the enhanced input system ready for input code to be added.
    - Added the Input Mapping Context & Assigned keys to InputActions.
    
    - Added InputActions for the following:
    - Forward Movement
    - Turret Rotation
    - Firing
    - Tank Rotation

--- Source/ToonTanks/Private/Tank.cpp
+++ Source/ToonTanks/Private/Tank.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Tank.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 
@@ -13,4 +16,38 @@ ATank::ATank()
 
     Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
     Camera->SetupAttachment(SpringArm);
+}
+
+void ATank::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // Add Input Mapping Context
+    if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+        {
+            SubSystem->AddMappingContext(MappingContext, 0);
+        }
+    }
+
+}
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+
+    }
+
 }
--- Source/ToonTanks/Public/Tank.h
+++ Source/ToonTanks/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "BasePawn.h"
+#include "InputActionValue.h"
 #include "Tank.generated.h"
 
 /**
@@ -17,9 +18,33 @@ class TOONTANKS_API ATank : public ABasePawn
 public:
 	ATank();
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 private:
+	// DEFAULT COMPONENTS
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 		class USpringArmComponent* SpringArm;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 		class UCameraComponent* Camera;
+
+	// ENHANCED INPUT SYSTEM
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputMappingContext* MappingContext;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputAction* InputMoveForward;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputAction* TurnInput;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputAction* RotateTurretInput;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputAction* FireInput;
+
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 };
